05d3a5c1ee8e769ea207a1a36ff350fee37ec52d
Rhushabh Bontapalle <rhushabhbontapallee@googlemail.com>
2026-03-05 11:31:08 -0500

    fix: satisfy Homebrew audit by removing redundant version

--- Formula/guardmycopy.rb
+++ Formula/guardmycopy.rb
@@ -2,7 +2,6 @@ class Guardmycopy < Formula
   desc "macOS clipboard firewall for reducing accidental secret pastes"
   homepage "https://github.com/rhushab/guardmycopy"
   url "https://github.com/rhushab/guardmycopy/archive/refs/tags/v1.0.0-rc2.tar.gz"
-  version "1.0.0-rc2"
   sha256 "2b4717f2cd2e7928f82c4728d2260bcfabb98d421638ddc98062c3013f2005e7"
   license "MIT"
 
